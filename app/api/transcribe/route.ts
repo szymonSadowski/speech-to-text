73bdf826400c9096d0a57650058cf9977347c7e2
Szymon Sadowski <szymon.sadowski@boldare.com>
2023-04-09 13:54:30 +0200

    feat: working mp3 to text

---
+++ app/api/transcribe/route.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { NextResponse } from "next/server";
+
+const MAX_FILE_SIZE = 25 * 1024 * 1024; // 50 MB in bytes
+const key = process.env.OPENAI_API_KEY;
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const file = formData.get("file") as File;
+  if (!file) {
+    throw new Error("No file provided");
+  }
+  if (file.length > MAX_FILE_SIZE) {
+    return new Response("File Too big", {
+      status: 413,
+    });
+  }
+  if (!key) {
+    return new Response("OpenAI API key not found", {
+      status: 500,
+    });
+  }
+  const formBody = new FormData();
+  formBody.append("file", file, "audio.mp3");
+  formBody.append("model", "whisper-1");
+  const response = await fetch(
+    "https://api.openai.com/v1/audio/transcriptions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${key}`,
+      },
+      body: formBody,
+    }
+  );
+  const data = await response.json();
+  return NextResponse.json({ data });
+}
